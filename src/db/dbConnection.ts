6b671365abd6e97b8c5330656281c34ed87d1789
DhanuramS <Dhanuram@mandlac.com>
2026-04-07 13:14:38 +0530

    updated suceeessfuly

--- src/db/dbConnection.ts
+++ src/db/dbConnection.ts
@@ -1,15 +1,22 @@
-import mongoose from "mongoose"
+import mongoose from 'mongoose';
 
-export const dbConnection= async():Promise<void>=>{
-try {
-    const dburl=process.env.MONGODB_URL
-    
-if(!dburl){
-    throw new Error('db url not fount')
-}
-await mongoose.connect(dburl).then(()=>{console.log('db connected');}).catch(()=>{console.log('db not connected');
-        })
-} catch (error) {
-console.error(error)
-}
-}
+export const dbConnection = async (): Promise<void> => {
+  try {
+    const dburl = process.env.MONGODB_URI;
+
+    if (!dburl) {
+      throw new Error('db url not fount');
+    }
+    await mongoose
+      .connect(dburl)
+      .then(() => {
+        console.log('db connected');
+      })
+      .catch((e) => {
+        console.log('db not connected');
+        console.log(e);
+      });
+  } catch (error) {
+    console.error(error);
+  }
+};
--- src/server.ts
+++ src/server.ts
@@ -1,33 +1,34 @@
-import express, { Request, Response } from 'express'
-import cors from 'cors'
-import dot from 'dotenv'
-import { dbConnection } from './db/dbConnection'
-import { allRouter } from './router/allRouter'
-dot.config()
-const app=express()
-app.use(cors())
-app.use(express.json())
-app.use(express.urlencoded({extended:true}))
+import express, { Request, Response } from 'express';
+import cors from 'cors';
+import dot from 'dotenv';
+import { dbConnection } from './db/dbConnection';
+import { allRouter } from './router/allRouter';
+dot.config();
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.use('/api/v1',allRouter)
+app.use('/api/v1', allRouter);
 
-app.get('/test',(resq:Request,res:Response)=>{
-    res.status(200).json({
-        message:'test server ok'
-    })
-})
+app.get('/test', (resq: Request, res: Response) => {
+  res.status(200).json({
+    message: 'test server ok',
+  });
+});
 
-const startServer=async():Promise<void>=>{
-    try {
+const startServer = async (): Promise<void> => {
+  try {
+    await dbConnection();
 
-        await dbConnection()
+    app.listen(process.env.PORT, () => {
+      console.log('server start');
+    });
+  } catch (error) {
+    console.error(error);
+  }
+};
 
-        app.listen(process.env.PORT,()=>{
-            console.log('server start');
-        })
-    } catch (error) {
-        console.error(error);
-    }
-}
+startServer();
 
-startServer()
+//db password=FD4sx4KCzCmN5GpM
