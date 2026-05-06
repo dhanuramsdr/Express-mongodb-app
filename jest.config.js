d3ccb1d1b272fb11381726e90b041f6229a546a6
dhanuramsdr <dhanuramseenivasan@gmail.com>
2026-05-06 04:23:52 -0400

    changes

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,18 @@
 export default {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  roots: ['<rootDir>/src'],
-  testMatch: ['**/*.test.ts'],
-  collectCoverageFrom: ['src/**/*.ts', '!src/**/*.test.ts', '!src/server.ts'],
+  roots: ['<rootDir>/src', '<rootDir>/tests'],
+  testMatch: ['**/__tests__/**/*.ts', '**/?(*.)+(spec|test).ts'],
+  collectCoverageFrom: [
+    'src/**/*.ts',
+    '!src/**/*.d.ts',
+    '!src/server.ts',
+    '!src/index.ts',
+  ],
   coverageDirectory: 'coverage',
-  coverageReporters: ['lcov', 'text', 'json'],
+  coverageReporters: ['text', 'lcov', 'html'],
+  moduleFileExtensions: ['ts', 'js', 'json'],
+  transform: {
+    '^.+\\.ts$': 'ts-jest',
+  },
 };
---
+++ tests/dummy.test.ts
@@ -0,0 +1,4 @@
+// Simple test to verify Jest works
+test('Jest is configured correctly', () => {
+  expect(true).toBe(true);
+});
